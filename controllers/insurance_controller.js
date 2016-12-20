297341f7281c41838a2b53028ddbcf3fc71b6770
olewis27 <omar.lewis@flhosp.org>
2016-12-20 12:08:04 -0500

    updated firebase

--- controllers/insurance_controller.js
+++ controllers/insurance_controller.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var router = express.Router();
 var path = require('path');
-// var firebase = require('firebase');
-// var app = firebase.initializeApp({
-//     apiKey: "AIzaSyA9DyjKvYnf3aLlpol8VSDWk1bhTUf6A9c",
-//     authDomain: "national-insurance-agency.firebaseapp.com",
-//     databaseURL: "https://national-insurance-agency.firebaseio.com",
-//     storageBucket: "national-insurance-agency.appspot.com",
-//     messagingSenderId: "800620644219"
-// });
+var firebase = require('firebase');
+var app = firebase.initializeApp({
+    apiKey: "AIzaSyA9DyjKvYnf3aLlpol8VSDWk1bhTUf6A9c",
+    authDomain: "national-insurance-agency.firebaseapp.com",
+    databaseURL: "https://national-insurance-agency.firebaseio.com",
+    storageBucket: "national-insurance-agency.appspot.com",
+    messagingSenderId: "800620644219"
+});
 
 
 
